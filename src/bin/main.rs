bc4fcabbd95d89338de6d07bafa923e7e8552afc
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2024-05-31 19:15:47 -0400

    Created a basic, single-threaded web server that returns a sample json for whiskey

---
+++ src/bin/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+
+    fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 512];
+
+        stream.read(&mut buffer).unwrap();
+
+        let contents = fs::read_to_string("temp-whiskeys.json").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
